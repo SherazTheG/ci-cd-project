43386c88b8f4c64d1c2335efd946ac31dc2eb185
Mohamed Sheraz M <mdsheraz102@gmail.com>
2025-11-11 15:08:27 +0530

    Update greeting message in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Hi, CI/CD!</h1>
+      <h1>Hi, I am Sheraz time is 15:08!</h1>
       <p>My React app is running from Jenkins!</p>
     </div>
   )
